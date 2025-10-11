17f54fcd0bb65e4bbc3129114f290ff26e842a1d
盧露 <low.lucyy@gmail.com>
2025-10-11 09:22:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,19 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 8080,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/ws': {
+        target: 'ws://localhost:5000',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
   resolve: {
     alias: {
